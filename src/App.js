1f4fdb219422fe1acd2330642b4a2103cd439572
jrodriguez <julian_ars1@hotmail.com>
2021-02-08 13:44:17 -0500

    Aplicacion donde se consume API para buscar imagen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Search from './components/Search';
+import Result from './components/Result';
 
-function App() {
+
+class App extends Component {
+
+  state = {
+    termino : '',
+    imagenes : [],
+    pagina : ''
+  }
+  //hace scroll hacia arriba de la pagina
+  scroll = () =>{
+    const elemento = document.querySelector('.jumbotron');
+    elemento.scrollIntoView('smooth','start');
+  }
+  //paginacion hacia adelante y atras
+  paginaAnterior = () =>{
+    
+    //leer el state
+    let pagina = this.state.pagina;
+    //si pagina es 1 no ir a tras
+    if(pagina ===1)return null;
+    //restar uno
+    pagina--;
+
+    //agregar la pagina en el state
+    this.setState({
+      pagina
+    },() =>{
+      this.consultarApi();
+      this.scroll();
+    });
+    console.log(pagina);
+  }
+  paginaSiguiente = () =>{
+    
+ //leer el state
+    let pagina = this.state.pagina;
+
+    //restar uno
+    pagina++;
+
+    //agregar la pagina en el state
+    this.setState({
+      pagina
+    }, () => {
+      this.consultarApi();
+      this.scroll();
+    });
+    console.log(pagina);
+  }
+  consultarApi = () =>{
+    const termino = this.state.termino;
+    const pagina = this.state.pagina;
+    const url = `https://pixabay.com/api/?key=20145731-f38a6776ff358f77afe12f7b0&q=${termino}&per_page=30&page=${pagina}`;
+    console.log(url);
+    fetch(url)
+      .then(response => response.json())
+      .then(date => this.setState({imagenes : date.hits}))
+  }
+
+  datosBusqueda = (termino)=>{
+    this.setState({
+      termino : termino,
+      pagina : 1
+    },()=>{
+        this.consultarApi();
+    })
+  }
+
+
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app container">
+      <div className="jumbotron">
+        <p className="lead text-center">Buscador de Imgenes</p>
+        <Search datosBusqueda={this.datosBusqueda}/>
+      </div>
+      <div className="row justify-content-center">
+        <Result imagenes={this.state.imagenes} paginaAnterior={this.paginaAnterior} paginaSiguiente={this.paginaSiguiente}  />
+      </div>      
     </div>
   );
+  }
 }
 
 export default App;
